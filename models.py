290de98939c2d2b7c14ff3b5e6e0b045fd3b97ca
Wisdom-oyemi <oyemiwisdom@gmail.com>
2026-03-11 22:27:09 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,4 @@
+# class Account - stores account info for users
+# class Menu - lists all items in MenuItem
+# class Transaction - handles item sales
+# class MenuItem - holds details for individual item
